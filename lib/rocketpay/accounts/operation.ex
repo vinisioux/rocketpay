4a35af74b4e8e801d0ff4232f334fc97e8ddb06a
ViniciusLeonadovky <vile.pb@gmail.com>
2021-02-25 10:16:10 -0300

    feat: transaction logic

--- lib/rocketpay/accounts/operation.ex
+++ lib/rocketpay/accounts/operation.ex
@@ -3,51 +3,51 @@ defmodule Rocketpay.Accounts.Operation do
 
   alias Rocketpay.{Account, Repo}
 
-def call(%{"id" => id, "value" => value}, operation) do
-  Multi.new()
-  |> Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
-  |> Multi.run(:update_balance, fn repo, %{account: account} ->
-    update_balance(repo, account, value, operation)
-  end)
-end
+  def call(%{"id" => id, "value" => value}, operation) do
+    operation_name = account_operation_name(operation)
+    Multi.new()
+    |> Multi.run(operation_name, fn repo, _changes -> get_account(repo, id) end)
+    |> Multi.run(operation, fn repo, changes ->
+      account = Map.get(changes, operation_name)
 
-defp get_account(repo, id) do
-  case repo.get(Account, id) do
-    nil -> {:error, "Account not found."}
-    account -> {:ok, account}
+      update_balance(repo, account, value, operation)
+    end)
   end
-end
 
-defp update_balance(repo, account, value, operation) do
-  account
-  |> operation(value, operation)
-  |> update_account(repo, account)
-end
-
-defp operation(%Account{balance: balance}, value, operation) do
-  value
-  |> Decimal.cast()
-  |> handle_cast(balance, operation)
-end
-
-defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, value)
-defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value)
-defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid deposit value."}
-
-defp update_account({:error, _reason} = error, _repo, _account), do: error
-
-defp update_account(value, repo, account) do
-  params = %{balance: value}
-
-  account
-  |> Account.changeset(params)
-  |> repo.update()
-end
-
-defp run_transaction(multi) do
-  case Repo.transaction(multi) do
-    {:error, _operation, reason, _changes} -> {:error, reason}
-    {:ok, %{update_balance: account}} -> {:ok, account}
+  defp get_account(repo, id) do
+    case repo.get(Account, id) do
+      nil -> {:error, "Account not found."}
+      account -> {:ok, account}
+    end
   end
-end
+
+  defp update_balance(repo, account, value, operation) do
+    account
+    |> operation(value, operation)
+    |> update_account(repo, account)
+  end
+
+  defp operation(%Account{balance: balance}, value, operation) do
+    value
+    |> Decimal.cast()
+    |> handle_cast(balance, operation)
+  end
+
+  defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, value)
+  defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value)
+  defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid deposit value."}
+
+  defp update_account({:error, _reason} = error, _repo, _account), do: error
+
+  defp update_account(value, repo, account) do
+    params = %{balance: value}
+
+    account
+    |> Account.changeset(params)
+    |> repo.update()
+  end
+
+  defp account_operation_name(operation) do
+    "account_#{Atom.to_string(operation)}" |> String.to_atom()
+  end
 end
