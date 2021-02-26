65c51f7b16128f8c8ee4569043cd76ab1b499b70
ViniciusLeonadovky <vile.pb@gmail.com>
2021-02-26 10:46:34 -0300

    authentication and tests

---
+++ test/rocketpay/users/create_test.exs
@@ -0,0 +1,39 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase, async: true
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, return an user" do
+      params = %{
+        name: "Vinicius",
+        password: "123456",
+        nickname: "sioux",
+        email: "vini@gmail.com",
+        age: 22
+      }
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "Vinicius", age: 22, id: ^user_id} = user
+    end
+
+    test "when all params are invalid, return an error" do
+      params = %{
+        name: "Vinicius",
+        password: "",
+        nickname: "sioux",
+        email: "vini@gmail.com",
+        age: 15
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{age: ["must be greater than or equal to 18"], password: ["can't be blank"]}
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,36 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Vinicius",
+      password: "123456",
+      nickname: "sioux",
+      email: "vini@gmail.com",
+      age: 22
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} = Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+      id: user_id,
+      name: "Vinicius",
+      nickname: "sioux"}
+    }
+
+    assert expected_response == response
+  end
+end
